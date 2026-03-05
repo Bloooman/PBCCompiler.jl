323cd21e390961e00ef1df69c31e0196d6e41905
Yuhan Gao <124081954+Bloooman@users.noreply.github.com>
2026-03-05 16:09:45 -0500

    Add function for generating random circuit with Pauli Product Rotations and Measurement

---
+++ src/Random_Circuit.jl
@@ -0,0 +1,29 @@
+"""
+Functions for generating random quantum circuits for testing purposes.
+"""
+
+
+function generate_random_circuit(num_ops::Int, num_qubits::Int)
+    ops = CircuitOp.Type[]
+    for i in 1:num_ops
+        op_type = rand(1:5)
+        qubits = sample((1:num_qubits), rand(1:num_qubits); replace=false, ordered=true)
+        p= random_pauli(length(qubits); nophase=true)
+        if op_type == 1
+            push!(ops, Measurement(p, rand([0,1]), qubits))
+        elseif op_type == 2
+            push!(ops, ExpHalfPiPauli(p, qubits))
+        elseif op_type == 3
+            push!(ops, ExpQuatPiPauli(p, qubits))
+        elseif op_type == 4
+            push!(ops, ExpEighPiPauli(p, qubits))
+        else
+            control_q=sample(qubits, rand(1:length(qubits)); replace=false, ordered=true)
+            target_q = setdiff(qubits, control_q)
+            control_p = random_pauli(length(control_q); nophase=true)
+            target_p = random_pauli(length(target_q); nophase=true)
+            push!(ops, PauliConditional(control_p, control_q, target_p, target_q))
+        end
+    end
+    return ops
+end
