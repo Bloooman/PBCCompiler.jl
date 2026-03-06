ba0e19c1cf0d3c3c2e1ade630fa16956619dfc7d
Yuhan Gao <124081954+Bloooman@users.noreply.github.com>
2026-03-06 14:59:59 -0500

    add tests for conjugation

--- test/test_conjugation.jl
+++ test/test_conjugation.jl
@@ -1,5 +1,28 @@
 using PBCCompiler
 using PBCCompiler: Circuit, CircuitOp, Measurement, ExpHalfPiPauli, ExpQuatPiPauli, ExpEighPiPauli, PauliConditional, BitConditional, affectedqubits
 using Test
-using PBCCompiler: conjugation
+using PBCCompiler: conjugate
 using QuantumClifford: @P_str
+using Moshi.Derive: @derive
+
+@derive CircuitOp[Eq, Show]
+
+@testset "Test unordered input" begin
+    op1=ExpQuatPiPauli(P"XY", [1, 3])
+    op2=ExpQuatPiPauli(P"ZXY", [3, 1, 2])
+    conjugated_op=ExpQuatPiPauli(P"-_YX", [1, 2, 3])
+
+    t_1=conjugate(op1,op2)
+    @test t_1 == (conjugated_op, op1)
+
+end
+
+@testset "Test non-overlapping input" begin
+    op1=ExpQuatPiPauli(P"X", [1])
+    op2=ExpQuatPiPauli(P"Z", [2])
+    conjugated_op=ExpQuatPiPauli(P"_Z", [1, 2])
+
+    t_1=conjugate(op1,op2)
+    @test t_1 == (conjugated_op, op1)
+
+end
