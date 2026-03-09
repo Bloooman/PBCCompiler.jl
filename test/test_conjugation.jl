d36becf1e6191410bd860ea9429fee0a130e5ee2
Yuhan Gao <124081954+Bloooman@users.noreply.github.com>
2026-03-09 19:33:28 -0400

    Add conjugation tests

--- test/test_conjugation.jl
+++ test/test_conjugation.jl
@@ -26,3 +26,66 @@ end
     @test t_1 == (conjugated_op, op1)
 
 end
+
+@testset "Test on single qubit CircuitOps" begin
+    op1=ExpQuatPiPauli(P"X", [1])
+    op2=ExpEighPiPauli(P"Z", [1])
+    op3=ExpHalfPiPauli(P"Y", [1])
+    conjugated_op_1=ExpEighPiPauli(P"Y", [1])
+    conjugated_op_2=ExpHalfPiPauli(P"-Z", [1])
+    conjugated_op_3=ExpQuatPiPauli(P"-X", [1])
+
+    t_1=conjugate(op1,op2)
+    @test t_1 == (conjugated_op_1, op1)
+    t_2=conjugate(op1,op3)
+    @test t_2 == (conjugated_op_2, op1)
+    t_3=conjugate(op3,op1)
+    @test t_3 == (conjugated_op_3, op3)
+
+end
+
+@testset "Test on PauliConditionals" begin
+    op1=ExpQuatPiPauli(P"X", [1])
+    op2=ExpQuatPiPauli(P"Z", [2])
+    CNOT=PauliConditional(P"Z", [1], P"X", [2])
+    conjugated_op_1=ExpQuatPiPauli(P"XX", [1, 2])
+    conjugated_op_2=ExpQuatPiPauli(P"ZZ", [1, 2])
+
+    t_1=conjugate(CNOT,op1)
+    @test t_1 == (conjugated_op_1, CNOT)
+
+    T_2=conjugate(CNOT,op2)
+    @test T_2 == (conjugated_op_2, CNOT)
+
+end
+
+@testset "Test on BitConditionals" begin
+    op1=ExpQuatPiPauli(P"X", [1])
+    Con_Z=BitConditional(ExpHalfPiPauli(P"Z", [1]), 1)
+
+    t_1=conjugate(Con_Z, op1)
+    @test t_1 === nothing
+
+end
+
+@testset "Test on Measurements" begin
+    op1=ExpQuatPiPauli(P"X", [2])
+    op2=ExpHalfPiPauli(P"X", [2])
+    CNOT=PauliConditional(P"Z", [1], P"X", [2])
+    M_Z=Measurement(P"Z", 1, [2])
+    conjugated_op_1=Measurement(P"_Y", 1, [1, 2])
+    conjugated_op_2=Measurement(P"-_Z", 1, [1, 2])
+    conjugated_op_3=Measurement(P"ZZ", 1, [1, 2])
+
+    t_1=conjugate(M_Z, op1)
+    @test t_1 === nothing
+
+    t_2=conjugate(op1, M_Z)
+    @test t_2 == (conjugated_op_1, op1)
+
+    t_3=conjugate(op2, M_Z)
+    @test t_3 == (conjugated_op_2, op2)
+
+    t_4=conjugate(CNOT, M_Z)
+    @test t_4 == (conjugated_op_3, CNOT)
+end
