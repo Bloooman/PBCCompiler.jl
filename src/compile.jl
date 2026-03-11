e7786c9008e737d3416cd8797660ea4412d1f9e0
Yuhan Gao <124081954+Bloooman@users.noreply.github.com>
2026-03-11 19:30:49 -0400

    Add pi/8 PPR traversal before measurement traversal

--- src/compile.jl
+++ src/compile.jl
@@ -12,8 +12,21 @@ function find_measurement_indices(circuit::Circuit)
     return measurement_indices
 end
 
+function find_nonclifford_indices(circuit::Circuit)
+    nonclifford_indices = []
+    for (index, op) in enumerate(circuit)
+        if isa_variant(op, CircuitOp.ExpEighPiPauli)
+            push!(nonclifford_indices, index)
+        end
+    end
+    return nonclifford_indices
+end
+
 function compilation(circuit::Circuit)
     validate_circuit(circuit)
+    for index in find_nonclifford_indices(circuit)
+        circuit=traversal(circuit, test_conjugate, :left, 1, index-1)
+    end
     for index in find_measurement_indices(circuit)
         circuit=traversal(circuit, test_conjugate, :left, 1, index-1)
     end
