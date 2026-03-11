02d4726e125c75f7bbd2ad91b0acc41b3d07b554
Yuhan Gao <124081954+Bloooman@users.noreply.github.com>
2026-03-11 19:32:08 -0400

    Updated random circuit generator to force all qubits being measured at the end of circuit

--- src/Random_Circuit.jl
+++ src/Random_Circuit.jl
@@ -16,16 +16,14 @@ A `Circuit` object containing the randomly generated operations.
 function generate_random_circuit(num_ops::Int, num_qubits::Int)
     ops = CircuitOp.Type[]
     for i in 1:num_ops
-        op_type = rand(1:5)
+        op_type = rand(1:4)
         qubits = sample((1:num_qubits), rand(1:num_qubits); replace=false, ordered=true)
-        p= random_pauli(length(qubits); nophase=true)
+        p= random_pauli(length(qubits); nophase=false)
         if op_type == 1
-            push!(ops, Measurement(p, rand([0,1]), qubits))
-        elseif op_type == 2
             push!(ops, ExpHalfPiPauli(p, qubits))
-        elseif op_type == 3
+        elseif op_type == 2
             push!(ops, ExpQuatPiPauli(p, qubits))
-        elseif op_type == 4
+        elseif op_type == 3
             push!(ops, ExpEighPiPauli(p, qubits))
         else
             control_q=sample(qubits, rand(1:length(qubits)); replace=false, ordered=true)
@@ -34,6 +32,11 @@ function generate_random_circuit(num_ops::Int, num_qubits::Int)
             target_p = random_pauli(length(target_q); nophase=true)
             push!(ops, PauliConditional(control_p, control_q, target_p, target_q))
         end
+    end
+    for i in 1:num_qubits
+        paulis = [P"X", P"Y", P"Z"]
+        p = rand(paulis)
+        push!(ops, Measurement(p, i, [i]))
     end
     return ops
 end
