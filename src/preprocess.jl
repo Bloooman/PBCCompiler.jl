fd0d9f5edb923625c4294ea9e26475051cb9c396
Yuhan Gao <124081954+Bloooman@users.noreply.github.com>
2026-03-12 21:53:04 -0400

    Add helper function for replacing nonclifford PPR with gadget

---
+++ src/preprocess.jl
@@ -0,0 +1,102 @@
+"""
+Functions for compiling quantum circuits by moving measurement operations to the beginning of the circuit.
+"""
+struct PauliQubitMismatchError <: Exception
+    msg::String
+end
+
+function validate_CircuitOp(op::CircuitOp.Type)
+    p=affectedpaulis(op)
+    q=affectedqubits(op)
+    name=variant_name(op)
+    if length(p) != length(q)
+        throw(PauliQubitMismatchError("$name($p, $q): The length of the Pauli string is not the same as the number of affected qubits. Please check the input operation."))
+    end
+end
+
+function validate_circuit(circuit::Circuit)
+    for op in circuit
+        validate_CircuitOp(op)
+    end
+end
+
+function get_circuit_width(circuit::Circuit)
+    width=0
+    for i in circuit
+        width=max(width,maximum(affectedqubits(i)))
+    end
+    return width
+end
+
+function get_bit_number(circuit::Circuit)
+    num_bit=0
+    for i in find_measurement_indices(circuit)
+        bits = @match circuit[i] begin
+            CircuitOp.Measurement(pauli, bit, qubits) => bit
+            _ => nothing
+        end
+        num_bit = max(num_bit,bits)
+    end
+    return num_bit
+end
+
+function find_measurement_indices(circuit::Circuit)
+    measurement_indices = []
+    for (index, op) in enumerate(circuit)
+        if isa_variant(op,CircuitOp.Measurement)
+            push!(measurement_indices, index)
+        end
+    end
+    return measurement_indices
+end
+
+function find_nonclifford_indices(circuit::Circuit)
+    nonclifford_indices = []
+    for (index, op) in enumerate(circuit)
+        if isa_variant(op, CircuitOp.ExpEighPiPauli)
+            push!(nonclifford_indices, index)
+        end
+    end
+    return nonclifford_indices
+end
+
+function remove_clifford(circuit::Circuit)
+    validate_circuit(circuit)
+    if find_nonclifford_indices(circuit) != []
+        for index in find_nonclifford_indices(circuit)
+            circuit=traversal(circuit, conjugate, :left, 1, index-1)
+        end
+    end
+    for index in find_measurement_indices(circuit)
+        circuit=traversal(circuit, conjugate, :left, 1, index-1)
+    end
+    return circuit
+end
+
+function gadgetize(circuit::Circuit)
+    num_input_qubit=get_circuit_width(circuit)
+    num_bit=get_bit_number(circuit)
+    num_magic_state=0
+    gadgetized_circuit=circuit
+    len=length(gadgetized_circuit)
+        for index in 1:len
+            op=circuit[index]
+            if isa_variant(op,CircuitOp.ExpEighPiPauli)
+                num_magic_state+=1
+                P=affectedpaulis(op)
+                Q=affectedqubits(op)
+                magic_state=[num_input_qubit+num_magic_state]
+                Pauli=tensor(P,P"Z")
+                Qubit=append!(Q,magic_state)
+                magic_bit_1=num_bit+2*num_magic_state-1
+                magic_bit_2=num_bit+2*num_magic_state
+                Measurement_1=CircuitOp.Measurement(Pauli,magic_bit_1,Qubit)
+                Measurement_2=CircuitOp.Measurement(P"X", magic_bit_2, magic_state)
+                BitConditional_1=CircuitOp.BitConditional(CircuitOp.ExpQuatPiPauli(P,Q),magic_bit_1)
+                BitConditional_2=CircuitOp.BitConditional(CircuitOp.ExpHalfPiPauli(P,Q),magic_bit_2)
+                gadget=[Measurement_1, Measurement_2, BitConditional_1, BitConditional_2]
+                splice!(gadgetized_circuit, index, gadget)
+            end
+        end
+    return (gadgetized_circuit, num_magic_state)
+end
