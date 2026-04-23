e37b8d3f0a8859ca825054f618380df9f68634aa
Yuhan Gao <124081954+Bloooman@users.noreply.github.com>
2026-04-23 19:05:43 -0400

    Add get_graph function to generate interaction graph between qubits involved in pauli product measurements

--- src/statistics.jl
+++ src/statistics.jl
@@ -1,3 +1,6 @@
+using Graphs, SimpleWeightedGraphs
+using QuantumClifford: xbit, zbit
+
 function get_distribution(input_circuit::Circuit, input_state::Stabilizer, num_shots::Int=1000)
     num_bits = get_circuit_width(input_circuit)
     len = 2^num_bits
@@ -16,4 +19,33 @@ function get_distribution(input_circuit::Circuit, input_state::Stabilizer, num_s
         i+=1
     end
     return (distribution, data)
+end
+
+function get_graph(result::ComputerState, type::Union{MeasurementResultType.Type,Nothing}=nothing)
+    num_nodes=maximum(vcat(result.memory_state.pauli_qubits,result.memory_state.magic_qubits))
+    g=SimpleWeightedGraph(UInt8(num_nodes))
+    measurements = type === nothing ?
+        result.memory_state.measurement_results :
+        filter(mr -> mr.result_type == type, result.memory_state.measurement_results)
+    for m in measurements
+        p=m.pauli
+        for i in 1:length(p)
+            @debug(i)
+            if xbit(p)[i] || zbit(p)[i]
+                for j in i+1:length(p)
+                    @debug(j)
+                    if xbit(p)[j] || zbit(p)[j]
+                        current_w = get_weight(g, i, j)
+                        add_edge!(g,i,j,current_w + 1)
+                    end
+                end
+            end
+        end
+    end
+    if type == QuantumRes()
+        for v in result.memory_state.pauli_qubits
+            rem_vertex!(g,v)
+        end
+    end
+    return g
 end
